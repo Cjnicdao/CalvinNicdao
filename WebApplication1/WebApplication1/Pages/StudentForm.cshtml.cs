38c5962da67a428ce935f1fe2cf91dbca558bec7
KALVEN\user <cjnicdao@csmbataan.ph.education>
2022-09-03 08:34:53 +0800

    NewProgram

---
+++ WebApplication1/WebApplication1/Pages/StudentForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class StudentFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
